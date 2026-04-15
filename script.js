ae87806a6b5a64439da6e156779b43babbc64354
Alma <isakvent@icloud.com>
2026-04-15 11:39:52 +0200

    positioned add to trolley button

---
+++ script.js
@@ -0,0 +1,7 @@
+const addToTrolleyButton = document.getElementById("addToTrolleyButton");
+
+addToTrolleyButton.addEventListener("click", handleAddToTrolley);
+
+function handleAddToTrolley() {
+    console.log("add to trolley");
+}
