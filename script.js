2883d218d77b3ef56fb6d01e0cc6f58368bfdb56
Alma <isakvent@icloud.com>
2026-04-15 16:18:56 +0200

    added simulation of adding item to trolley

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
 const addToTrolleyButton = document.getElementById("addToTrolleyButton");
+const inTrolleyIcon = document.getElementById("inTrolleyIcon");
 
 addToTrolleyButton.addEventListener("click", handleAddToTrolley);
 
 function handleAddToTrolley() {
     console.log("add to trolley");
+    inTrolleyIcon.classList.remove("hidden");
 }
